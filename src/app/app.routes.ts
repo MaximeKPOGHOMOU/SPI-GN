deff99fd9a5585820b2104722d1e2e9a7ca7eadd
Maxime KPOGHOMOU <maximekpoghomou18@gmail.com>
2026-02-03 14:25:37 +0000

    Fonctionnalite

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,51 +1,22 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { About } from './about/about';
+import { Equipe } from './equipe/equipe';
 import { Service } from './service/service';
 import { Contact } from './contact/contact';
-import { Equipe } from './equipe/equipe';
-import { Home } from './home/home';
-import { Agents } from './admin/agents/agents';
-import { Clients } from './admin/clients/clients';
-import { Sites } from './admin/sites/sites';
-import { Equipements } from './admin/equipements/equipements';
-import { Dotations } from './admin/dotations/dotations';
-import { Dashboard } from './admin/dashboard/dashboard';
-import { Layout } from './admin/layout/layout';
-import { Affectations } from './admin/affectations/affectations';
-import { Login } from './admin/login/login';
-import { StaffGuard } from './admin/guards/staff.guard';
-import { AuthGuard } from './admin/guards/auth.guard';
-
 
 export const routes: Routes = [
 
-  // 🌍 SITE VITRINE (PAR DÉFAUT)
-  { path: '', component: Home },
+  // 🔁 REDIRECTION AU LANCEMENT
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  // 🌍 SITE VITRINE
+  { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: 'equipe', component: Equipe },
   { path: 'service', component: Service },
   { path: 'contact', component: Contact },
 
-  // 🔐 LOGIN
-  { path: 'login', component: Login },
-
-  // 🛠️ ADMIN (STAFF)
-  {
-    path: 'admin',
-    component: Layout,
-    canActivate: [AuthGuard, StaffGuard],
-    children: [
-      { path: 'dashboard', component: Dashboard },
-      { path: 'agent', component: Agents },
-      { path: 'client', component: Clients },
-      { path: 'site', component: Sites },
-      { path: 'equipement', component: Equipements },
-      { path: 'dotation', component: Dotations },
-      { path: 'affectation', component: Affectations },
-    ]
-  },
-
-  // fallback
-  { path: '**', redirectTo: '' }
+  // ❌ ROUTE INCONNUE → HOME
+  { path: '**', redirectTo: 'home' }
 ];
-
